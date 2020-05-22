959a301d626c7e0bc36a5a4f0c49ec3a5b118472
Nic <nickypoo.is@gmail.com>
2020-05-23 00:34:42 +1200

    Updates

---
+++ precache-manifest.a225ac4d77f133748403707225addd1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77d542d8461fde3595193d657b8b3666",
+    "url": "/monsters-roladex/index.html"
+  },
+  {
+    "revision": "9a7d10d4ae93f8aa2f30",
+    "url": "/monsters-roladex/static/css/main.d266410f.chunk.css"
+  },
+  {
+    "revision": "9d36cb1448d0cd9bbf34",
+    "url": "/monsters-roladex/static/js/2.9d52131b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-roladex/static/js/2.9d52131b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a7d10d4ae93f8aa2f30",
+    "url": "/monsters-roladex/static/js/main.5ed03a3c.chunk.js"
+  },
+  {
+    "revision": "ce80dadaecda6863a58a",
+    "url": "/monsters-roladex/static/js/runtime-main.43d7ebee.js"
+  }
+]);
